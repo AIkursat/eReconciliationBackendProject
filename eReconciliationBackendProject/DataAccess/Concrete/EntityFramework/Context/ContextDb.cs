f61c2b79e7a20835b97978438d0f672c47bdde12
AIKursat <aynurkursat1999@hotmail.com>
2022-09-16 20:55:46 +0300

    Create the context

--- eReconciliationBackendProject/DataAccess/Concrete/EntityFramework/Context/ContextDb.cs
+++ eReconciliationBackendProject/DataAccess/Concrete/EntityFramework/Context/ContextDb.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,24 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework.Context
 {
-    public class ContextDb
+    public class ContextDb : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=KURSAT\MSSQLSERVER01;Database=eReconciliationDb;Integrated Security=True");
+        }
+
+        public DbSet<AccountReconciliationDetail> accountReconciliationDetails { get; set; }
+        public DbSet<AccountReconciliations> AccountReconciliations { get; set; }
+        public DbSet<BaBsReconciliation> baBsReconciliations { get; set; }
+        public DbSet<BaBsReconciliationDetails> BaBsReconciliationDetails { get; set; }
+        public DbSet<Company> companies { get; set; }
+        public DbSet<Currency> currencies { get; set; }
+        public DbSet<CurrencyAccount> currencyAccounts { get; set; }
+        public DbSet<MailParameter> mailParameters { get; set; }
+        public DbSet<OperationClaim> operationClaims { get; set; }
+        public DbSet<User> users { get; set; }
+        public DbSet<UserCompany> userCompanies { get; set; }
+        public DbSet<UserOperationClaim> userOperationClaims { get; set; }
     }
 }
