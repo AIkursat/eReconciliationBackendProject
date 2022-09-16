94d44a84e7a38dcddfd59bf628d05c8625412ded
AIKursat <aynurkursat1999@hotmail.com>
2022-09-16 20:07:16 +0300

    Create Context and entities

---
+++ eReconciliationBackendProject/Entities/Concrete/AccountReconciliations.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class AccountReconciliations : IEntity
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public int CurrencyAccountId { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime EndingDate { get; set; }
+        public int CurrencyId { get; set; }
+        public decimal CurrencyDebit { get; set; }
+        public bool IsSendEmail { get; set; }
+        public DateTime? SendEmailDate { get; set; }
+        public bool? IsEmailRead { get; set; }
+        public DateTime? EmailReadDate { get; set; }
+        public bool? IsResultSucced { get; set; }
+        public DateTime? ResultDate { get; set; }
+        public string? ResultNote { get; set; }
+    }
+}
