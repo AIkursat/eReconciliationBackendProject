8e81e76ffd6dd45d154454f06894f6c658dad3d4
AIKursat <aynurkursat1999@hotmail.com>
2022-09-17 17:58:47 +0300

    Generate Generic structure

---
+++ eReconciliationBackendProject/Core/DataAccess/EntityFramework/EFEntityFrameworkBase.cs
@@ -0,0 +1,62 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EFEntityFrameworkBase<TEntity, TContext> : IEntityRepository<TEntity> where TEntity : class, IEntity, new() where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            using(var context = new TContext())
+            {
+                var AddedEntity = context.Entry(entity);
+                AddedEntity.State = EntityState.Added;  
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var DeletedEntity = context.Entry(entity);
+                DeletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (var context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefaultAsync(filter);
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (var context = new TContext())
+            {
+                return filter == null
+                ? context.Set<TEntity>().ToList()
+                : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                var UpdateEntity = context.Entry(entity);
+                UpdateEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ eReconciliationBackendProject/DataAccess/Abstract/ICompanyDal.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface ICompanyDal
+    {
+        void Add(Company company);
+        void Update(Company company);
+        void Delete(Company company);
+        List<Company> GetAll();
+        Company Get(int id); // get one record
+    }
+}
