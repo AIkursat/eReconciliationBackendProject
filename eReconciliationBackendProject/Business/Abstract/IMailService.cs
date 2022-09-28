8eadd020e77c5141eae18190980be02d2026cbaf
AIKursat <aynurkursat1999@hotmail.com>
2022-09-28 17:42:56 +0300

    Create generic mail

---
+++ eReconciliationBackendProject/Business/Abstract/IMailService.cs
@@ -0,0 +1,14 @@
+﻿using Core.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IMailService
+    {
+        IResult SendMail(string mail, string subject, string body);
+    }
+}
---
+++ eReconciliationBackendProject/Business/Concrete/MailManager.cs
@@ -0,0 +1,18 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class MailManager : IMailService
+    {
+        public IResult SendMail(string mail, string subject, string body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
